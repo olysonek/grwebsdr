300c7443fb40798259bec815e6e6bbbb04ece77a
Ondřej Lysoněk <xlysonek@fi.muni.cz>
2016-10-26 14:07:12 +0200

    Use shorter types

--- main.cpp
+++ main.cpp
@@ -31,21 +31,21 @@ public:
 
 int main()
 {
-	shared_ptr<osmosdr::source> src = osmosdr::source::make();
+	osmosdr::source::sptr src = osmosdr::source::make();
 	osmosdr::meta_range_t range = src->get_sample_rates();
-	shared_ptr<rational_resampler_base_fff> resampler1 =
+	rational_resampler_base_fff::sptr resampler1 =
 			rational_resampler_base_fff::make(48, 50,
 			std::vector<float>());
-	shared_ptr<rational_resampler_base_ccc> resampler2 =
+	rational_resampler_base_ccc::sptr resampler2 =
 			rational_resampler_base_ccc::make(1, 4,
 			std::vector<gr_complex>());
-	shared_ptr<fir_filter_ccf> low_pass = fir_filter_ccf::make(1,
+	fir_filter_ccf::sptr low_pass = fir_filter_ccf::make(1,
 			firdes::low_pass(1.0, 2000000.0, 100000.0, 1000000.0));
-	shared_ptr<fir_filter_fff> low_pass2 = fir_filter_fff::make(10.0,
+	fir_filter_fff::sptr low_pass2 = fir_filter_fff::make(10.0,
 			firdes::low_pass(1.0, 500000.0, 50000.0/2-50000.0/32, 50000.0/32));
-	shared_ptr<quadrature_demod_cf> demod = quadrature_demod_cf::make(
+	quadrature_demod_cf::sptr demod = quadrature_demod_cf::make(
 			500000/(2*M_PI*75000));
-	shared_ptr<audio::sink> sink = audio::sink::make(48000);
+	audio::sink::sptr sink = audio::sink::make(48000);
 
 	std::cout << range.to_pp_string() << std::endl;
 	std::cout << "New sample rate: " << src->set_sample_rate(2000000.0)
@@ -59,7 +59,7 @@ int main()
 	src->set_bb_gain(20.0);
 	src->set_bandwidth(0.0);
 
-	shared_ptr<top_block> bl = make_top_block("bla");
+	top_block_sptr bl = make_top_block("bla");
 	bl->connect(src, 0, resampler2, 0);
 	bl->connect(resampler2, 0, low_pass, 0);
 	bl->connect(low_pass, 0, demod, 0);
