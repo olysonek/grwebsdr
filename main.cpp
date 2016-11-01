8b985c0cfb37be6fda221dd4d5f4534786084276
Ondřej Lysoněk <xlysonek@fi.muni.cz>
2016-11-01 18:30:44 +0100

    Use freq_xlating_filter

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <gnuradio/filter/firdes.h>
 #include <gnuradio/filter/rational_resampler_base_ccc.h>
 #include <gnuradio/filter/rational_resampler_base_fff.h>
+#include <gnuradio/filter/freq_xlating_fir_filter_ccc.h>
 #include <gnuradio/top_block.h>
 #include <iostream>
 #include <cmath>
@@ -49,22 +50,29 @@ std::vector<gr_complex> filter_c(int interpolation, int decimation, double fbw)
 	return ret;
 }
 
+std::vector<gr_complex> taps_f2c(std::vector<float> vec)
+{
+	std::vector<gr_complex> ret;
+
+	ret.reserve(vec.size());
+	for (float i : vec)
+		ret.push_back(gr_complex(i, 0.0));
+	return ret;
+}
+
 int main()
 {
 	double src_rate = 2000000.0;
-	int dec1 = 10; // Pre-demodulation decimation
+	int dec1 = 8; // Pre-demodulation decimation
 	double dec1_rate = src_rate / dec1; // Sample rate after first decimation
 	int dec2 = dec1_rate / 1000; // Decimate down to 1kHz
 
 	osmosdr::source::sptr src = osmosdr::source::make();
 
-	rational_resampler_base_ccc::sptr resampler1 =
-			rational_resampler_base_ccc::make(1, dec1,
-			filter_c(1, dec1, 0.4f));
-
-	fir_filter_ccf::sptr low_pass1 = fir_filter_ccf::make(1,
-			firdes::low_pass(1.0, dec1_rate,
-			dec1_rate / 2 - dec1_rate / 1000, dec1_rate / 1000));
+	freq_xlating_fir_filter_ccc::sptr xlate =
+			freq_xlating_fir_filter_ccc::make(dec1,
+			taps_f2c(firdes::low_pass(1.0, src_rate, 75000, 25000)),
+			0.0, src_rate);
 
 	quadrature_demod_cf::sptr demod = quadrature_demod_cf::make(
 			dec1_rate /(2*M_PI*75000));
@@ -91,9 +99,8 @@ int main()
 	src->set_bandwidth(0.0);
 
 	top_block_sptr bl = make_top_block("bla");
-	bl->connect(src, 0, resampler1, 0);
-	bl->connect(resampler1, 0, low_pass1, 0);
-	bl->connect(low_pass1, 0, demod, 0);
+	bl->connect(src, 0, xlate, 0);
+	bl->connect(xlate, 0, demod, 0);
 	bl->connect(demod, 0, low_pass2, 0);
 	bl->connect(low_pass2, 0, resampler2, 0);
 	bl->connect(resampler2, 0, sink, 0);
