19f06e0c3e87de76db0fb868cadbe78295924385
Ondřej Lysoněk <xlysonek@fi.muni.cz>
2016-11-19 17:49:24 +0100

    Base classes

---
+++ src/band.py
@@ -0,0 +1,18 @@
+from gnuradio import filter
+from gnuradio.filter import firdes
+
+class band:
+    def __init__(self, source, top_block, center_freq, cutoff):
+        self._source = source
+        self._top_block = top_block
+        self._center_freq = center_freq
+        self._cutoff = cutoff
+        self._decimation = 8
+        taps = firdes.low_pass(1.0, source._output.get_sample_rate(), cutoff,
+                cutoff / 3, firdes.WIN_HAMMING, 6.76)
+        self._output = filter.freq_xlating_fir_filter_ccc(self._decimation,
+                taps, center_freq, source._output.get_sample_rate())
+        top_block.connect((source, 0), (self._output, 0))
+
+    def get_output_rate(self):
+
---
+++ src/raw_audio.py
@@ -0,0 +1,23 @@
+from gnuradio import filter
+from gnuradio import analog
+
+class raw_audio:
+    def __init__(self, source, top_block, demod):
+        self._source = source
+        self._top_block = top_block
+        self._demod = demod
+
+        if demod == "FM":
+            self._demodulator = analog.wfm_rcv(quad_rate=source.get_output_rate(),
+                    audio_decimation=1)
+        else:
+            self._demodulator = None
+
+        self._output = filter.rational_resampler_fff(interpolation=48,
+                decimation=250, taps=None, fractional_bw=None)
+
+        top_block.connect((source, 0), (self._demodulator, 0))
+        top_block.connect((self._demodulator, 0), (self._output, 0))
+
+    def get_output_rate(self):
+        return 48000
