4551760bb00b88177a9b797723dfa6eaf356c66f
Ondřej Lysoněk <xlysonek@fi.muni.cz>
2016-11-21 10:58:21 +0100

    Change center freq through WebSocket

--- src/py/band.py
+++ src/py/band.py
@@ -8,11 +8,19 @@ class band:
         self._center_freq = center_freq
         self._cutoff = cutoff
         self._decimation = 8
-        taps = firdes.low_pass(1.0, source._output.get_sample_rate(), cutoff,
+        self._taps = firdes.low_pass(1.0, source._output.get_sample_rate(), cutoff,
                 cutoff / 3, firdes.WIN_HAMMING, 6.76)
-        self._output = filter.freq_xlating_fir_filter_ccc(self._decimation,
-                taps, center_freq, source._output.get_sample_rate())
+        self.setup_filter(center_freq)
         top_block.connect((source._output, 0), (self._output, 0))
+
+    def setup_filter(self, freq):
+        self._center_freq = freq
+        self._output = filter.freq_xlating_fir_filter_ccc(self._decimation,
+                self._taps, freq, self._source._output.get_sample_rate())
 
     def get_output_rate(self):
         return self._source._output.get_sample_rate() / self._decimation
+
+    def set_center_freq(self, freq):
+        print('Changing center frequency to ' + str(freq) + ' Hz')
+        self._output.set_center_freq(freq)
