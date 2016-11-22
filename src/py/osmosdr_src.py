c9164a913af41bc4046cfd8f057f36df6ffc22c3
Ondřej Lysoněk <xlysonek@fi.muni.cz>
2016-11-22 18:17:51 +0100

    Change center freq through a websocket

--- src/py/osmosdr_src.py
+++ src/py/osmosdr_src.py
@@ -5,7 +5,7 @@ class osmosdr_src:
         self._id = id
         self._output = osmosdr.source(args=id)
         self._output.set_sample_rate(2e6)
-        self._output.set_center_freq(951e5)
+        self._output.set_center_freq(926e5)
         self._output.set_freq_corr(0)
         self._output.set_dc_offset_mode(0)
         self._output.set_iq_balance_mode(0)
