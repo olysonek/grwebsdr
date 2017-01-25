bb888d2942396d6ecfb3a53fa47d115fb26f24d5
Ondřej Lysoněk <olysonek@redhat.com>
2017-01-25 21:37:22 +0100

    Get rid of the rubbish

--- src/py/sink.py
+++
@@ -1,8 +0,0 @@
-from gnuradio import audio
-
-class sink:
-    def __init__(self, source, top_block):
-        self._source = source
-        self._top_block = top_block
-        self._sink = audio.sink(source.get_output_rate(), '', True)
-        top_block.connect((source._output, 0), (self._sink, 0))
