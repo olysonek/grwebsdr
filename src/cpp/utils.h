7dcbdb1f25394f26c661fdab9114c65e59438c81
Ondřej Lysoněk <olysonek@redhat.com>
2017-02-18 10:22:42 +0100

    Remove filter_f and filter_c

--- src/cpp/utils.h
+++ src/cpp/utils.h
@@ -6,8 +6,6 @@
 #include <string>
 #include <gnuradio/gr_complex.h>
 
-std::vector<float> filter_f(int interpolation, int decimation, double fbw);
-std::vector<gr_complex> filter_c(int interpolation, int decimation, double fbw);
 std::vector<gr_complex> taps_f2c(std::vector<float> vec);
 int set_nonblock(int fd);
 int count_receivers_running();
