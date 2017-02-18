a6bb2bc30f971dd1fb0c7f528891319801622d69
Ondřej Lysoněk <olysonek@redhat.com>
2017-02-18 10:20:51 +0100

    Use low-pass instead of filter_f in fm_demod

--- src/cpp/fm_demod.cpp
+++ src/cpp/fm_demod.cpp
@@ -1,10 +1,12 @@
 #include "fm_demod.h"
 #include "utils.h"
 #include <boost/math/common_factor_rt.hpp>
+#include <vector>
 
 using namespace gr;
 using namespace gr::analog;
 using namespace gr::filter;
+using namespace std;
 
 fm_demod::sptr fm_demod::make(int in_rate, int out_rate, int cutoff, int out_cutoff, int out_trans)
 {
@@ -19,12 +21,13 @@ fm_demod::fm_demod(int in_rate, int out_rate, int cutoff, int out_cutoff, int ou
 	int div = boost::math::gcd(in_rate, out_rate);
 	int interpolation = out_rate / div;
 	int decimation = in_rate / div;
+	vector<float> taps;
 
 	demod = quadrature_demod_cf::make(in_rate / (2 * M_PI * cutoff));
-	low_pass = fir_filter_fff::make(1, firdes::low_pass(1.0, in_rate,
-				out_cutoff, out_trans));
+	taps = firdes::low_pass(1.0, in_rate, out_cutoff, out_trans);
+	low_pass = fir_filter_fff::make(1, taps);
 	resampler = rational_resampler_base_fff::make(interpolation, decimation,
-			filter_f(interpolation, decimation, 0.4f));
+			taps);
 
 	connect(self(), 0, demod, 0);
 	connect(demod, 0, low_pass, 0);
